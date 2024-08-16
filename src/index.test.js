00bdd5bfa68e7720b67ef3b2609ea74b24414b9c
George Perry <gperry@exploratorium.edu>
2024-08-15 21:06:02 -0700

    test performance improvements

--- src/index.test.js
+++ src/index.test.js
@@ -11,39 +11,6 @@ describe('drupalInterfaceTranslations', () => {
 });
 
 describe('examples', () => {
-  const beforeEachGroup = (group) => {
-    beforeEach(async () => {
-      await new Promise((resolve, reject) => {
-        exec(`npm install && npm run build`, (error) => {
-          if (error) {
-            reject(error);
-          }
-          resolve();
-        });
-      });
-
-      const nodeModules = `examples/${group}/node_modules`;
-      try {
-        await stat(nodeModules);
-        await rm(nodeModules, { recursive: true });
-      } catch (error) {
-        if (error.code !== 'ENOENT') {
-          throw error;
-        }
-      }
-
-      const packageLock = `examples/${group}/package-lock.json`;
-      try {
-        await stat(nodeModules);
-        await rm(packageLock);
-      } catch (error) {
-        if (error.code !== 'ENOENT') {
-          throw error;
-        }
-      }
-    });
-  };
-
   const testCanBuild = (group) => {
     test(`can build the "${group}" example`, async () => {
       await new Promise((resolve, reject) => {
@@ -61,16 +28,41 @@ describe('examples', () => {
       const potFilePath = `examples/${group}/translations/${group}.pot`;
       const content = await readFile(potFilePath, 'utf8');
       expect(content).toMatch(/\nmsgid "Vite logo"\n/);
-    });
+    }, 10_000);
   };
 
-  describe('react', () => {
-    beforeEachGroup('react');
-    testCanBuild('react');
+  beforeAll(async () => {
+    await new Promise((resolve, reject) => {
+      exec(`npm install && npm run build`, (error) => {
+        if (error) {
+          reject(error);
+        }
+        resolve();
+      });
+    });
+
+    for (const group in ['react', 'vanilla']) {
+      const nodeModules = `examples/${group}/node_modules`;
+      try {
+        await stat(nodeModules);
+        await rm(nodeModules, { recursive: true, force: true });
+      } catch (error) {
+        if (error.code !== 'ENOENT') {
+          throw error;
+        }
+      }
+
+      const packageLock = `examples/${group}/package-lock.json`;
+      try {
+        await stat(packageLock);
+        await rm(packageLock);
+      } catch (error) {
+        if (error.code !== 'ENOENT') {
+          throw error;
+        }
+      }
+    }
   });
 
-  describe('vanilla', () => {
-    beforeEachGroup('vanilla');
-    testCanBuild('vanilla');
-  });
+  ['react', 'vanilla'].forEach(testCanBuild);
 });
